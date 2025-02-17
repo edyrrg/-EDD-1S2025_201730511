624a87fe37965ec5a5d91311395d6c45c91fc43f
Edy Rojas <edyrojas64@gmail.com>
2025-02-16 20:44:40 -0600

    Agregar proyecto inicial de Fase1 con configuración de .NET 8 y una ventana de saludo en GtkSharp

---
+++ AutoGest_Pro/Fase1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Gtk;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Init();
+
+        var win = new Window("Hello World");
+        win.SetDefaultSize(200, 200);
+        win.DeleteEvent += (_, _) => Application.Quit();
+
+        var label = new Label("Hello World");
+        win.Add(label);
+
+        win.ShowAll();
+
+        Application.Run();
+    }
+}
