81e19a42b3d6b7b52c48eccd19ecfc75a0fbb917
Edy Rojas <edyrojas64@gmail.com>
2025-02-23 01:16:29 -0600

    Agregar funcionalidad de lista simple para gestión de usuarios y eliminar modelos innecesarios

--- AutoGest_Pro/Fase1/Program.cs
+++ AutoGest_Pro/Fase1/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using Fase1.src.adt;
 using Fase1.src.gui;
+using Fase1.src.models;
 using Gtk;
 
 namespace Fase1
@@ -10,9 +12,26 @@ namespace Fase1
     {
         static void Main(string[] args)
         {
-            Application.Init();
-            _ = new Login();
-            Application.Run();
+            // Application.Init();
+            // _ = new Login();
+            // Application.Run();
+            var SimpleList = new SimpleList<int>();
+            SimpleList.Insert(1, "Juan", "Perez", "jperez@mail.com", "1234");
+            SimpleList.Insert(2, "Maria", "Lopez", "mlopez@mail.com", "5678");
+
+            Console.WriteLine("Lista de Usuarios");
+            SimpleList.Print();
+
+            Console.WriteLine("Update status response: " + SimpleList.Udpate(2, "Maria", "Lopez", "mlopez@gmail.com", "5678"));
+
+            Console.WriteLine("Lista de Usuarios");
+            SimpleList.Print();
+
+            Console.WriteLine("Update status response: " + SimpleList.Delete(2));
+            
+            Console.WriteLine("Lista de Usuarios");
+            SimpleList.Print();
+
         }
     }
 }
